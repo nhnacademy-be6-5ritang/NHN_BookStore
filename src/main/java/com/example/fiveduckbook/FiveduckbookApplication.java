d6b21d49be3d3bdebd715e7245bff6cc38a5bb7b
Jung_Hun_Chae <50732828+chaesc1@users.noreply.github.com>
2024-06-11 15:42:31 +0900

    초기 프로젝트 설정

---
+++ src/main/java/com/example/fiveduckbook/FiveduckbookApplication.java
@@ -0,0 +1,13 @@
+package com.example.fiveduckbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiveduckbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiveduckbookApplication.class, args);
+	}
+
+}
